b522f2892e6a8b482a7b93fb9a10de0f13953a89
aasaashishranjan2 <aasaashishranjan2@gmail.com>
2019-07-16 02:28:53 +0200

    code fix

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { Test1Component } from './test1/test1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Test1Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/test1/test1.component.ts
@@ -0,0 +1,72 @@
+import {Component, OnInit} from '@angular/core';
+import * as randomWords from 'random-words';
+
+export interface IEntry {
+  id: number;
+  name: string;
+  description: string;
+  status: string;
+}
+
+@Component({
+  selector: 'app-test1',
+  templateUrl: './test1.component.html',
+  styleUrls: ['./test1.component.css']
+})
+export class Test1Component implements OnInit {
+
+  data: IEntry[];
+  page = 1;
+  pageSize = 20;
+  emptyArray = new Array(1000);
+
+  filteredData: IEntry[];
+  previous = false;
+  constructor() {
+  }
+
+  ngOnInit() {
+
+    this.data = [];
+
+   
+    for (let i = 0; i < 10; i++) {
+      this.data.push({
+        id: i,
+        name: randomWords({exactly: 3, join: ' '}),
+        description: randomWords({exactly: 100, join: ' '}),
+        status: ['new', 'submitted', 'failed'][Math.floor(Math.random() * 3)]
+      });
+    }
+    this.filteredData = this.data;
+    this.populateFilteredData('');
+
+  }
+
+  populateFilteredData(filter) {
+    this.filteredData = [];
+    let filterArr = filter.split(' ');
+   filterArr.map ((filter) => {
+
+    const filteredTempData = this.data.filter((entry) => {
+          return (entry.name.indexOf(filter) !== -1 || entry.description.indexOf(filter) !== -1 || entry.status.indexOf(filter) !== -1);
+           
+        });
+        this.filteredData = this.filteredData.concat(filteredTempData);
+      }) 
+  }
+
+  updateFilter($event: KeyboardEvent) {
+    this.populateFilteredData(($event.target as HTMLInputElement).value);
+  }
+
+  showNext() {
+    this.page++;
+
+    this.previous = this.page === 1 ? false: true;
+  }
+  showPrevious() {
+    this.page--;
+    this.previous = this.page === 1 ? false: true;
+  }
+}
